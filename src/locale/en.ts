bf1c6467821a40f95b0e7a8b35b2e7b85e7ab2d1
qiang <qw13131wang@gmail.com>
2025-07-12 12:28:26 +0800

    chore: optimize i18n (#3)

--- src/locale/en.ts
+++ src/locale/en.ts
@@ -1,13 +1,9 @@
 export default {
   'en': {
     'updating.dependencies': 'Updating dependencies with {name}...',
-    'upgrading.dependencies': 'Upgrading dependencies with {name}...',
     'lock.file.changed': 'Detected {name} file changes. Update dependencies?',
-    'lock.file.changed.auto': 'Detected {name} file changes. Auto updating dependencies...',
     'update': 'Update',
     'later': 'Later',
-    'update.completed': 'Dependencies updated successfully!',
-    'update.failed': 'Failed to update dependencies',
     'upgrade.completed': 'Dependencies upgraded successfully!',
     'upgrade.failed': 'Failed to upgrade dependencies',
     'open.workspace.first': 'Please open a workspace first'
